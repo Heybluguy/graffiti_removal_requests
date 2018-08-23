fa68089fd9c3e01e31001122a85edb954fbcba58
Luis  Senior <heybluguy@yahoo.com>
2018-08-23 12:57:52 -0500

    Clean up.

--- app/models/graffiti_request_service.rb
+++ app/models/graffiti_request_service.rb
@@ -1,5 +1,4 @@
 class GraffitiRequestService
-
   def get_requests(month, year, alderman)
     response = connection.get("?ward=#{alderman.ward}&$where=creation_date between '#{year}-#{month}-01T00:00:01' and '#{year}-#{month.succ}-01T00:00:00'")
     if response_ok?(response)
